168fad1891926888e3ae2ee727bf3d1ab65c043f
Andrew G. Brown <andrew.g.brown@usda.gov>
2020-08-22 20:15:12 -0700

    SoilTaxonomy Cypher-generator

--- KST/neo4j_cypher/make_SoilTaxonomy_cypher.R
+++ KST/neo4j_cypher/make_SoilTaxonomy_cypher.R
@@ -1,37 +1,39 @@
-taxa <- read.csv("soiltaxonomy_12th_db_codes.csv")
+taxa <- read.csv(file = "../KSTLookup/soiltaxonomy_12th_db_codes.csv")
+taxa$nice_name <- taxa$name
 taxa$name <- gsub(" |\\-","", taxa$name)
+
 oid <- 0
-taxset <- c("CREATE (Soil:Taxonomy {title:'Soil Taxonomy', edition:12})",
-            lapply(taxa$code[nchar(taxa$code) == 1], function(taxoncode) {
-   tsub <- subset(taxa, code == taxoncode)
+taxset <- lapply(taxa$code[nchar(taxa$code) == 1], function(taxoncode) {
+   # soil order counter
    oid <<- oid + 1
-   paste0(sprintf("CREATE (%s:Taxon {title:'%s', code:'%s'})", tsub$name, tsub$name, tsub$code),"\n",
-   sprintf("CREATE (%s)-[:CHILD_OF{nth: %s}]->(%s)", tsub$name, oid, "Soil"))
-  }))
 
-taxset <- c(taxset, lapply(taxa$code[nchar(taxa$code) >= 2], function(taxoncode) {
-  tsub <- subset(taxa, code == taxoncode)
-  sprintf("CREATE (%s:Taxon {title:'%s', code:'%s'})", tsub$name, tsub$name, tsub$code)
-}))
+   tsub <- subset(taxa, code == taxoncode)
 
-taxrelationset <- lapply(unique(taxa$code)[nchar(taxa$code) >= 2], function(taxoncode) {
-  buf <-  ""
-  tsub <- subset(taxa, code == taxoncode)
-  tchild <- subset(taxa, grepl(paste0("^",taxoncode), code))
-  tchild <- subset(tchild, code != taxoncode)
-  if(nchar(taxoncode) < 3)
-    tchild <- subset(tchild, nchar(code) <= nchar(taxoncode) + 1)
-  if(nrow(tchild)) {
-    buf <- paste0(buf, "CREATE\n")
-    for(i in 1:nrow(tchild)) {
-      buf <- paste0(buf, sprintf("(%s)-[:CHILD_OF{nth:%s}]->(%s)", tchild[i,]$name, i, tsub$name))
-      if(i == nrow(tchild))
-        buf <- paste0(buf, ";")
-      else buf <- paste0(buf, ",\n")
+   tchild <- subset(taxa, grepl(paste0("^",taxoncode), code))
+   tchild <- subset(tchild, code != taxoncode)
+   tchild <- tchild[order(nchar(tchild$code), tchild$code),]
+
+   level <- c("order","suborder","great group","subgroup","subgroup")
+   tchild$taxlevel <- level[nchar(tchild$code)]
+   newid <- substr(tchild$code, 0, nchar(tchild$code) - ifelse(nchar(tchild$code) == 5, 2, 1))
+   lut <- taxa$name
+   names(lut) <- taxa$code
+   tchild$parent <- lut[newid]
+   tchild$parentcode <- newid
+   res <- lapply(split(tchild, f = factor(tchild$parent,
+                                          levels = unique(tchild$parent),
+                                          ordered = TRUE)), function(x) {
+    buf <- ""
+    for(i in 1:nrow(x)){
+      tsubsub <- x[i,]
+      buf <- paste0(buf, sprintf("CREATE (%s:Taxon {title:'%s', code:'%s', taxlevel:'%s', keypos:%s, parent:'%s', parentcode:'%s'})-[:CHILD_OF]->(%s)\n",
+              tsubsub$name, tsubsub$nice_name, tsubsub$code, tsubsub$taxlevel, i, tsubsub$parent, tsubsub$parentcode, tsubsub$parent))
     }
-  }
-  return(buf)
+    return(buf)
+   })
+
+   paste0(sprintf("CREATE (%s:Taxon {title:'%s', code:'%s', taxlevel:'order', keypos:%s, parent:'Soil', parentcode:'*'})-[:CHILD_OF]->(Soil)\n", tsub$name, tsub$name, tsub$code, oid),
+          do.call("paste0", res))
 })
 
-write(do.call('c', taxset), "SoilTaxonomy.cypher")
-write(do.call('c', taxrelationset), "SoilTaxonomy.cypher", append = TRUE)
+write(do.call('c', c("CREATE (Soil:Taxonomy {title:'Soil Taxonomy', edition:12})", taxset)), "SoilTaxonomy.cypher")
